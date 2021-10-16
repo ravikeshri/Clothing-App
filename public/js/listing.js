ec73c25db4b3507d3c1519d76a68f7a77be59d4c
Ashish Kujur <ashish.xenon@gmail.com>
2021-10-16 14:36:12 +0530

    Linked product catalog and products to backend.
    - Product categories can now be toggled in the listing page.
    
    - Data loaded in product page. Product pages respond to new color selection and image toggle.

--- public/js/listing.js
+++ public/js/listing.js
@@ -9,3 +9,39 @@ $(".category-list div").click(function(){
   clicked_element.eq(0).addClass("active");
   clicked_element.eq(0).find(".cat-icon").addClass("active");
 });
+
+function categoryFind()
+{
+  let x = window.location.href.split('/');
+  let category = x[x.length - 1];
+  if(category == "tshirts")
+  {
+    $('.category-list > div').eq(0).addClass('active');
+    $('.category-list > div').eq(0).children().eq(0).addClass('active');
+  }
+  if(category == "shirts")
+  {
+    $('.category-list > div').eq(1).addClass('active');
+    $('.category-list > div').eq(1).children().eq(0).addClass('active');
+  }
+  if(category == "bottoms")
+  {
+    $('.category-list > div').eq(2).addClass('active');
+    $('.category-list > div').eq(2).children().eq(0).addClass('active');
+  }
+  if(category == "outerwears")
+  {
+    $('.category-list > div').eq(3).addClass('active');
+    $('.category-list > div').eq(3).children().eq(0).addClass('active');
+  }
+  if(category == "knitwears")
+  {
+    $('.category-list > div').eq(4).addClass('active');
+    $('.category-list > div').eq(4).children().eq(0).addClass('active');
+  }
+  if(category == "accessories")
+  {
+    $('.category-list > div').eq(5).addClass('active');
+    $('.category-list > div').eq(5).children().eq(0).addClass('active');
+  }
+}
